c168908d6c685dd1b12ac42c2c386b938939415b
Scott Trinkle <tscott.trinkle@gmail.com>
2021-08-07 10:53:18 -0500

    Adds connectivity matrix and spearman matrix viewers

--- braingraphgeo/utils.py
+++ braingraphgeo/utils.py
@@ -1,25 +1,123 @@
 import os
+import numpy as np
 import pandas as pd
+from scipy.stats import spearmanr
 
 
-def load_data(path):
+def load_connectivity_matrix(path, return_both_hemispheres=True):
     '''
-    Utility function to load graph adjacency matrix.
+    Utility function to load graph connectivity matrices.
 
     Parameters
     __________
     path : str
         Path to data file. Must be .csv
+    return_both_hemispheres : bool
+        Bool. If false, only returns the first half of the rows, assuming
+        the matrix was constructed with hemispheric symmetry.
 
     Returns
     _______
     df : DataFrame
         Adjacency matrix with node labels as pandas DataFrame
-
-
     '''
     if os.path.splitext(path)[1] != '.csv':
         raise ValueError('Please enter the path to a .csv file')
 
     df = pd.read_csv(path, index_col=0)
-    return df
+
+    if return_both_hemispheres:
+        return df
+    else:
+        n = df.shape[0]//2
+        return df.iloc[:n]
+
+
+def build_spearman_correlation_matrix(mat1, mat2, parcellation):
+    '''
+    Takes two connectivity matrices and generates a matrix
+    of spearman correlation coefficients between the 12 major brain
+    divisions.
+
+    Parameters
+    __________
+    mat1 : ndarray
+        2D connectivity matrix. Assumes hemispheric symmetry with "sources"
+        as rows, and ipsi- and contralateral "targets" as columns,
+        respectively.
+    mat2 : ndarray
+        2D connectivity matrix. Assumes hemispheric symmetry with "sources"
+        as rows, and ipsi- and contralateral "targets" as columns,
+        respectively.
+    parcellation : pandas DataFrame
+        Must contain a row for every node and a "Brain Division" column
+        with strings matching the names of one of the 12 major brain
+        divisions listed in the Allen atlas.
+
+    Returns
+    _______
+    corr : ndarray
+        2D matrix of shape (12,24) corresponding to spearman correlations
+        between 12 major brain divisions. corr[:,:12] corresponds to
+        ipsilateral connections and cprr[:,12:] corresponds to contralateral
+        connections. Hemispheric symmetry is assumed.
+    '''
+
+    # Check inputs
+    check_parcellation(parcellation)
+
+    if mat1.shape != mat1.shape:
+        raise ValueError('mat1 and mat2 do not have the same shape')
+    N_nodes = mat1.shape[1]
+    N_per_hemi = N_nodes//2
+
+    brain_divisions = ['Isocortex', 'OLF', 'HPF', 'CTXsp', 'STR',
+                       'PAL', 'TH', 'HY', 'MB', 'P', 'MY', 'CB']
+    N_div = len(brain_divisions)
+
+    # Maps nodes to brain divisions
+    masks = {}
+    for division in brain_divisions:
+        masks[division] = np.array(parcellation['Brain Division'] == division)
+
+    corr = np.zeros((N_div, N_div*2))
+    for i, div_i in enumerate(brain_divisions):
+        for j, div_j in enumerate(brain_divisions):
+            mask = np.outer(masks[div_i], masks[div_j])  # per hemisphere
+
+            # ipsilateral on left
+            corr[i, j] = spearmanr(mat1[:, :N_per_hemi]
+                                   [mask], mat2[:, :N_per_hemi][mask])[0]
+            # contralateral on right
+            corr[i, j+N_div] = spearmanr(mat1[:, N_per_hemi:]
+                                         [mask], mat2[:, N_per_hemi:][mask])[0]
+    return corr
+
+
+def check_parcellation(parcellation):
+    '''
+    Utility function to check that the user-supplied parcellation data
+    is compatible with the Allen brain divisions.
+
+    Parameters
+    __________
+    parcellation : pandas DataFrame
+        Must contain a row for every node and a "Brain Division" column
+        with strings matching the names of one of the 12 major brain
+        divisions listed in the Allen atlas.
+
+    Raises
+    _______
+    ValueError
+        If `parcellation` contains a brain division label that is not one
+        of the 12 major brain divisions from the Allen atlas
+    '''
+
+    brain_divisions = ['Isocortex', 'OLF', 'HPF', 'CTXsp', 'STR',
+                       'PAL', 'TH', 'HY', 'MB', 'P', 'MY', 'CB']
+    # Brain division acronyms in parcellation
+    parcel_acros = parcellation['Brain Division'].unique()
+    for parcel_acro in parcel_acros:
+        if parcel_acro not in brain_divisions:
+            raise ValueError(
+                f'Parcellation contains unknown acronym: {parcel_acro}')
---
+++ braingraphgeo/vis.py
@@ -0,0 +1,261 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib import cm
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from .utils import check_parcellation
+
+COLORS = {'ipsi': '#d4d8de',
+          'contra': '#c0c4cb',
+          'source': '#c0c4cb',
+          'Isocortex': '#97d3bb',
+          'OLF': '#9dd08c',
+          'HPF': '#07a56e',
+          'CTXsp': '#b7dcb9',
+          'STR': '#c7daf0',
+          'PAL': '#7aa0d4',
+          'TH': '#ee512c',
+          'HY': '#f58e8d',
+          'MB': '#8568ae',
+          'P': '#f47836',
+          'MY': '#f7e2e5',
+          'CB':  '#f5ee74'}
+
+TEXT = {'ipsi': 'Ipsilateral Target',
+        'contra': 'Contralateral Target',
+        'source': 'Source',
+        'Isocortex': 'ICTX',
+        'OLF': 'OLF',
+        'HPF': 'HPF',
+        'CTXsp': 'CTXsp',
+        'STR': 'STR',
+        'PAL': 'PAL',
+        'TH': 'TH',
+        'HY': 'HY',
+        'MB': 'MB',
+        'P': 'P',
+        'MY': 'MY',
+        'CB':  'CB'}
+
+
+def label_rect(rect, text, ax, rotation=0):
+    '''
+    Utility function for labeling a Rectangle object.
+
+    Parameters
+    __________
+    rect : matplotlib.patches.Rectangle
+        Rectangle object to be labeled
+    text : str
+        Label text
+    ax : matplotlib.axes._subplots.AxesSubplot
+        Figure ax
+    rotation : float
+        Rotation angle for label. Default is 0.
+
+    Returns
+    _______
+    ax : matplotlib.axes._subplots.AxesSubplot
+        Figure ax
+    '''
+    ax.add_patch(rect)
+    rx, ry = rect.get_xy()
+    cx = rx + rect.get_width() / 2.0
+    cy = ry + rect.get_height() / 2.0
+    ax.annotate(text, (cx, cy), ha='center', va='center',
+                rotation=rotation)
+
+    return ax
+
+
+def connectivity_matrix(data, parcellation, vmin, vmax,
+                        ticklabels=None, cmap='inferno'):
+    '''
+    Function for generating a labeled connectivity matrix figure
+    in a similar style to Figure 3 in "A mesoscale connectome of
+    the mouse brain" (Oh et al, 2014). Note that this assumes
+
+    Parameters
+    __________
+    data : ndarray
+        2D connectivity matrix. Assumes hemispheric symmetry with "sources"
+        as rows, and ipsi- and contralateral "targets" as columns,
+        respectively.
+    parcellation : pandas DataFrame
+        Must contain a row for every node and a "Brain Division" column
+        with strings matching the names of one of the 12 major brain
+        divisions listed in the Allen atlas.
+    vmin : float
+        Minimum display value
+    vmax : float
+        Maximum display value
+    ticklabels : list-like of length 2
+        Custom ticklabels for colorbar. Defaults to [vmin,vmax]
+    cmap : str
+        Matplotlib colormap. Default is 'inferno'
+
+
+    Returns
+    _______
+    fig : matplotlib.figure.Figure
+        Matplotlib figure
+    ax : matplotlib.axes._subplots.AxesSubplot
+        Figure ax
+    '''
+
+    # Error if division labels aren't as expected
+    check_parcellation(parcellation)
+
+    # 12 major brain divisions in Allen Atlas
+    brain_divisions = ['Isocortex', 'OLF', 'HPF', 'CTXsp', 'STR',
+                       'PAL', 'TH', 'HY', 'MB', 'P', 'MY', 'CB']
+
+    # Maps nodes to brain divisions
+    masks = {}
+    for division in brain_divisions:
+        masks[division] = np.array(parcellation['Brain Division'] == division)
+    ny, nx = data.shape
+
+    # Get mpl colormap and set nan/inf values to black
+    cmap = getattr(cm, cmap).with_extremes(bad='k')
+
+    # Pad matrix to include labels
+    hemilabel_dx = 20
+    structlabel_dx = 40
+    buff = hemilabel_dx + structlabel_dx
+    mat = np.zeros((ny + buff, nx + buff))
+    mat[buff:, buff:] = data
+
+    # Initialize figure
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.set_axis_off()
+
+    # Display data
+    im = ax.imshow(mat, cmap=cmap, vmin=vmin, vmax=vmax)
+
+    # Add gray blocks for contra/ipsi/source and add text
+    ipsi_rect = patches.Rectangle(xy=(buff-0.5, -0.5),
+                                  width=nx // 2,
+                                  height=hemilabel_dx,
+                                  facecolor=COLORS['ipsi'])
+    contra_rect = patches.Rectangle(xy=(buff + nx//2 - 0.5, -0.5),
+                                    width=nx // 2,
+                                    height=hemilabel_dx,
+                                    facecolor=COLORS['contra'])
+    source_rect = patches.Rectangle(xy=(-0.5, buff-0.5),
+                                    width=hemilabel_dx,
+                                    height=ny,
+                                    facecolor=COLORS['source'])
+    label_rect(ipsi_rect, TEXT['ipsi'], ax)
+    label_rect(contra_rect, TEXT['contra'], ax)
+    label_rect(source_rect, TEXT['source'], ax, rotation=90)
+
+    # Adds white rect for colorbar
+    corner_rect = patches.Rectangle(xy=(-0.5, -0.5), width=buff,
+                                    height=buff, facecolor='white')
+    ax.add_patch(corner_rect)
+
+    # Add colored patch labels for each brain division
+    for division in brain_divisions:
+        src_rect = ax.add_patch(
+            patches.Rectangle(xy=(hemilabel_dx - 0.5,
+                                  buff-0.5 + np.where(masks[division])[0][0]),
+                              width=structlabel_dx,
+                              height=masks[division].sum(
+            ),
+                facecolor=COLORS[division]))
+        label_rect(src_rect, TEXT[division], ax)
+
+        # Rotate so they will fit
+        if division in ['OLF', 'HPF', 'CTXsp', 'STR', 'PAL']:
+            rotation = 90
+        else:
+            rotation = 0
+
+        tgt_r_rect = ax.add_patch(
+            patches.Rectangle(xy=(buff - 0.5 + np.where(masks[division])[0][0],
+                                  hemilabel_dx-0.5),
+                              width=masks[division].sum(
+            ),
+                height=structlabel_dx,
+                facecolor=COLORS[division]))
+        label_rect(tgt_r_rect, TEXT[division], ax, rotation=rotation)
+
+        tgt_l_rect = ax.add_patch(
+            patches.Rectangle(xy=(nx // 2 + buff - 0.5 + np.where(masks[division])[0][0],
+                                  hemilabel_dx-0.5),
+                              width=masks[division].sum(
+            ),
+                height=structlabel_dx,
+                facecolor=COLORS[division]))
+
+        label_rect(tgt_l_rect, TEXT[division], ax, rotation=rotation)
+
+    fig.tight_layout()
+
+    # Add colorbar
+    cbax = inset_axes(ax, width="75%", height="20%", loc='center',
+                      bbox_to_anchor=corner_rect.get_bbox(),
+                      bbox_transform=ax.transData,
+                      borderpad=0)
+    cbar = fig.colorbar(im, cax=cbax, orientation='horizontal',
+                        ticks=[vmin, vmax])
+    if ticklabels is not None:
+        cbar.ax.set_xticklabels(ticklabels, fontdict={'fontsize': 13})
+    cbar.ax.set_title(r'Log$_{10}$', fontsize=13)
+
+    return fig, ax
+
+
+def plot_spearman_correlation_matrix(corr):
+    '''
+    Function for generating a labeled heatmap of spearman correlation
+    values between the 12 major brain divisions defined in the Allen
+    Atlas. 
+
+    Parameters
+    __________
+    corr : ndarray
+        Correlation matrix (shape=(12,24)) between two brain graphs. 
+        Assumes hemispheric symmetry and that there are values for all 12
+        divisions.
+
+    Returns
+    _______
+    fig : matplotlib.figure.Figure
+        Matplotlib figure
+    ax : matplotlib.axes._subplots.AxesSubplot
+        Figure ax
+    '''
+
+    # Check shape
+    if corr.shape != (12, 24):
+        raise ValueError('Incorrect brain divisions')
+
+    # Make division text labels
+    division_labs = ['ICTX', 'OLF', 'HPF', 'CTXsp', 'STR', 'PAL', 'TH',
+                     'HY', 'MB', 'P', 'MY', 'CB']
+    i_labs = [lab + '-I' for lab in division_labs]
+    c_labs = [lab + '-C' for lab in division_labs]
+    target_labs = i_labs + c_labs
+
+    fig, ax = plt.subplots(figsize=(14, 8))
+    ax = sns.heatmap(data=corr,
+                     vmin=-1,
+                     vmax=1,
+                     center=0,
+                     cmap='coolwarm',
+                     cbar=True,
+                     cbar_kws={'shrink': 0.7},
+                     annot=False,
+                     square=True,
+                     ax=ax)
+    ax.axvline(12, color='k', lw=2.5)
+    ax.xaxis.tick_top()
+    ax.set_xticklabels(target_labs, rotation=45, fontsize=20)
+    ax.set_yticklabels(division_labs, rotation=0, fontsize=20)
+    cbar = ax.collections[0].colorbar
+    cbar.ax.tick_params(labelsize=18)
+    fig.tight_layout()
+    return fig, ax
